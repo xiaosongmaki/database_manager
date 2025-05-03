a17b526c86c51b3dcf3463ef1e2b990f2c159886
HUANG X <xinghuang738@gmail.com>
2025-05-03 17:36:27 +0900

    feat: 增加了对prefect的支持

--- src/config/env.py
+++ src/config/env.py
@@ -6,7 +6,7 @@ load_dotenv()
 
 # MySQL配置
 MYSQL_HOST = os.getenv('MYSQL_HOST')
-MYSQL_PORT = int(os.getenv('MYSQL_PORT'))
+MYSQL_PORT = int(os.getenv('MYSQL_PORT',3306))
 MYSQL_USER = os.getenv('MYSQL_USER')
 MYSQL_PASSWORD = os.getenv('MYSQL_PASSWORD')
 MYSQL_DATABASE = os.getenv('MYSQL_DATABASE')
